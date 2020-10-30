0c0ac298ffe41bb59203454ec8512bc17b98112d
Blerim Abdullai <66042567+bl33m@users.noreply.github.com>
2020-10-29 22:37:12 -0500

    Delete test.py

--- simpleMod/Xbox/test.py
+++
@@ -1,9 +0,0 @@
-import xbox
-joy = xbox.Joystick()         #Initialize joystick
-
-prevXval = 0
-while not joy.Back():
-    currXval = joy.rightX
-    if currXval >= prevXval:
-        prevXval = currXval 
-        print (joy.rightX())
